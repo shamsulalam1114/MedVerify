4e5217d207acebc38c80d486821a2316c3175b0a
MD.SHAMSUL ALAM <113705486+shamsulalam1114@users.noreply.github.com>
2025-12-24 21:31:05 +0600

    some modification on session application

--- Controllers/family_profile_session.php
+++ Controllers/family_profile_session.php
@@ -1,8 +1,8 @@
 <?php
     session_start();
 
-    if(!isset($_SESSION['status']) || $_SESSION['status'] !== true){
-        header('location: ../Views/login.php');
-        exit();
+    if(isset($_SESSION['status']) !== true){
+        header('location: login.html');
     }
+
 ?>
