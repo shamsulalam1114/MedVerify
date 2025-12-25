b6a420f6b1369d6de859b801d65e50286d60a6ff
MD.SHAMSUL ALAM <113705486+shamsulalam1114@users.noreply.github.com>
2025-12-25 15:20:36 +0600

    added some modifications on features in the calander section in the dashboard page

--- Views/calendar.php
+++ Views/calendar.php
@@ -19,7 +19,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Today's Schedule</title>
+    <title>My Appointments</title>
     <link rel="stylesheet" href="../Assets/dashboard.css">
     <script src="../Assets/calendar.js"></script>
 </head>
@@ -49,7 +49,7 @@
         <table width="100%">
             <tr>
                 <td align="center">
-                    <h2>Today's Schedule - December 25, 2025</h2>
+                    <h2>My Appointments</h2>
                 </td>
             </tr>
         </table>
@@ -60,74 +60,26 @@
         <table width="100%">
             <tr>
                 <td align="center">
-                    <h3>Today's Appointments</h3>
+                    <h3>Upcoming Appointments</h3>
                 </td>
             </tr>
         </table>
 
-        <table border="1" width="100%" id="todayAppointmentsTable">
-            <tr>
-                <th>Time</th>
-                <th>Doctor/Lab</th>
-                <th>Type</th>
-                <th>Status</th>
-                <th>Action</th>
-            </tr>
-            <tr>
-                <td>10:00 AM</td>
-                <td>Dr. Ahmed Khan</td>
-                <td>General Checkup</td>
-                <td>Confirmed</td>
-                <td><button type="button" onclick="alert('Viewing appointment details')">View</button></td>
-            </tr>
-            <tr>
-                <td>02:30 PM</td>
-                <td>City Lab</td>
-                <td>Blood Test</td>
-                <td>Pending</td>
-                <td><button type="button" onclick="alert('Viewing appointment details')">View</button></td>
-            </tr>
-        </table>
-
-        <br><br>
-
-        
-        <table width="100%">
-            <tr>
-                <td align="center">
-                    <h3>Upcoming This Week</h3>
-                </td>
-            </tr>
-        </table>
-
-        <table border="1" width="100%" id="weekAppointmentsTable">
+        <table border="1" width="100%" id="appointmentsTable">
             <tr>
                 <th>Date</th>
-                <th>Time</th>
                 <th>Doctor/Lab</th>
                 <th>Type</th>
-                <th>Status</th>
             </tr>
             <tr>
                 <td>Dec 26, 2025</td>
-                <td>11:00 AM</td>
-                <td>Dr. Sarah Ahmed</td>
-                <td>Follow-up</td>
-                <td>Confirmed</td>
+                <td>Dr. Ahmed</td>
+                <td>Checkup</td>
             </tr>
             <tr>
                 <td>Dec 28, 2025</td>
-                <td>09:00 AM</td>
-                <td>Heart Clinic</td>
-                <td>ECG Test</td>
-                <td>Scheduled</td>
-            </tr>
-            <tr>
-                <td>Dec 30, 2025</td>
-                <td>03:00 PM</td>
-                <td>Dr. Rahman</td>
-                <td>Consultation</td>
-                <td>Confirmed</td>
+                <td>City Lab</td>
+                <td>Blood Test</td>
             </tr>
         </table>
 
@@ -137,49 +89,7 @@
         <table width="100%">
             <tr>
                 <td align="center">
-                    <h3>Past Appointments</h3>
-                </td>
-            </tr>
-        </table>
-
-        <table border="1" width="100%" id="pastAppointmentsTable">
-            <tr>
-                <th>Date</th>
-                <th>Time</th>
-                <th>Doctor/Lab</th>
-                <th>Type</th>
-                <th>Status</th>
-            </tr>
-            <tr>
-                <td>Dec 20, 2025</td>
-                <td>10:30 AM</td>
-                <td>Dr. Karim</td>
-                <td>Consultation</td>
-                <td>Completed</td>
-            </tr>
-            <tr>
-                <td>Dec 15, 2025</td>
-                <td>02:00 PM</td>
-                <td>ABC Lab</td>
-                <td>X-Ray</td>
-                <td>Completed</td>
-            </tr>
-            <tr>
-                <td>Dec 10, 2025</td>
-                <td>11:00 AM</td>
-                <td>Dr. Hasan</td>
-                <td>Follow-up</td>
-                <td>Completed</td>
-            </tr>
-        </table>
-
-        <br><br>
-
-        
-        <table width="100%">
-            <tr>
-                <td align="center">
-                    <h3>Schedule New Appointment</h3>
+                    <h3>Add New Appointment</h3>
                 </td>
             </tr>
         </table>
@@ -187,11 +97,7 @@
         <table border="1" width="100%">
             <tr>
                 <td width="30%">Date:</td>
-                <td width="70%"><input type="date" id="appointmentDate" style="width: 100%"></td>
-            </tr>
-            <tr>
-                <td>Time:</td>
-                <td><input type="time" id="appointmentTime" style="width: 100%"></td>
+                <td width="70%"><input type="text" id="appointmentDate" placeholder="dd-mm-yyyy" style="width: 100%"></td>
             </tr>
             <tr>
                 <td>Doctor/Lab:</td>
@@ -208,7 +114,7 @@
         <table width="100%">
             <tr>
                 <td align="center">
-                    <button type="button" id="addAppointmentBtn">Schedule Appointment</button>
+                    <button type="button" id="addAppointmentBtn">Add Appointment</button>
                     <button type="button" id="clearFormBtn">Clear Form</button>
                 </td>
             </tr>
