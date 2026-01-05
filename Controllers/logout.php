cf5b373e506963096c0c7d805a1dbf7ed27fdb2c
MD.SHAMSUL ALAM <113705486+shamsulalam1114@users.noreply.github.com>
2026-01-05 22:00:18 +0600

    login logout issue regarding to session

--- Controllers/logout.php
+++ Controllers/logout.php
@@ -1,9 +1,7 @@
 <?php
-
-    //session_start();
-    //session_destroy();
-    //unset($_SESSION['status']);
-
+    session_start();
+    session_destroy();
+    
     setcookie('status', 'true', time()-10, '/');
     header('location: ../Views/login.php');
 
