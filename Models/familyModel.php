1aeca1bdc2b0f22e56b6a91d8110f5d113f33a3a
MD.SHAMSUL ALAM <113705486+shamsulalam1114@users.noreply.github.com>
2026-01-05 18:38:26 +0600

    database connection and CURD updated

---
+++ Models/familyModel.php
@@ -0,0 +1,73 @@
+<?php
+require_once('db.php');
+
+
+function getFamilyMembers($user_id){
+    $con = getConnection();
+    $sql = "select * from family_members where user_id='$user_id'";
+    $result = mysqli_query($con, $sql);
+    
+    $members = [];
+    while($row = mysqli_fetch_assoc($result)){
+        array_push($members, $row);
+    }
+    
+    return $members;
+}
+
+function getFamilyMemberById($id){
+    $con = getConnection();
+    $sql = "select * from family_members where member_id='$id'";
+    $result = mysqli_query($con, $sql);
+    
+    if(mysqli_num_rows($result) > 0){
+        $row = mysqli_fetch_assoc($result);
+        return $row;
+    }else{
+        return false;
+    }
+}
+
+function getFamilyMemberCount($user_id){
+    $con = getConnection();
+    $sql = "select * from family_members where user_id='$user_id'";
+    $result = mysqli_query($con, $sql);
+    
+    $count = mysqli_num_rows($result);
+    return $count;
+}
+
+function addFamilyMember($member){
+    $con = getConnection();
+    $sql = "insert into family_members values(null, '{$member['user_id']}', '{$member['name']}', '{$member['relationship']}', '{$member['age']}', '{$member['blood_group']}', null)";
+    
+    if(mysqli_query($con, $sql)){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function deleteFamilyMember($id){
+    $con = getConnection();
+    $sql = "delete from family_members where member_id='$id'";
+    
+    if(mysqli_query($con, $sql)){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function updateFamilyMember($member){
+    $con = getConnection();
+    $sql = "update family_members set name='{$member['name']}', relationship='{$member['relationship']}', age='{$member['age']}', blood_group='{$member['blood_group']}' where member_id='{$member['member_id']}'";
+    
+    if(mysqli_query($con, $sql)){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+?>
---
+++ Models/userModel.php
@@ -0,0 +1,77 @@
+<?php
+require_once('db.php');
+
+
+function login($user){
+    $con = getConnection();
+    $sql = "select * from users where username='{$user['username']}' and password='{$user['password']}'";
+    $result = mysqli_query($con, $sql);   
+    
+    if(mysqli_num_rows($result) == 1){
+        $row = mysqli_fetch_assoc($result);
+        return $row;
+    }else{
+        return false;
+    }
+}
+
+function getUserById($id){
+    $con = getConnection();
+    $sql = "select * from users where user_id='$id'";
+    $result = mysqli_query($con, $sql);
+    
+    if(mysqli_num_rows($result) > 0){
+        $row = mysqli_fetch_assoc($result);
+        return $row;
+    }else{
+        return false;
+    }
+}
+
+function getAllUsers(){
+    $con = getConnection();
+    $sql = "select * from users";
+    $result = mysqli_query($con, $sql);
+    
+    $users = [];
+    while($row = mysqli_fetch_assoc($result)){
+        array_push($users, $row);
+    }
+    
+    return $users;
+}
+
+function addUser($user){
+    $con = getConnection();
+    $sql = "insert into users values(null, '{$user['username']}', '{$user['password']}', '{$user['email']}', '{$user['full_name']}', '{$user['blood_group']}', '{$user['age']}', null)";
+    
+    if(mysqli_query($con, $sql)){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function deleteUser($id){
+    $con = getConnection();
+    $sql = "delete from users where user_id='$id'";
+    
+    if(mysqli_query($con, $sql)){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function updateUser($user){
+    $con = getConnection();
+    $sql = "update users set username='{$user['username']}', password='{$user['password']}', email='{$user['email']}', full_name='{$user['full_name']}', blood_group='{$user['blood_group']}', age='{$user['age']}' where user_id='{$user['user_id']}'";
+    
+    if(mysqli_query($con, $sql)){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+?>
