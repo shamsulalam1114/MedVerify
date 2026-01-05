197e1a62312e8f68698cb5906c739d0630bde26a
MD.SHAMSUL ALAM <113705486+shamsulalam1114@users.noreply.github.com>
2026-01-05 21:53:01 +0600

    session anmd cookie errors solved

--- Controllers/view_reports_session.php
+++ Controllers/view_reports_session.php
@@ -3,6 +3,12 @@
 
     if(isset($_COOKIE['status']) !== true){
         header('location: ../Views/login.php');
+        exit();
+    }
+    
+    if(!isset($_SESSION['username'])){
+        header('location: ../Views/login.php');
+        exit();
     }
 
     //print_r($_SERVER);
