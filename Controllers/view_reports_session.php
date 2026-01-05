cddc41d37092dcc784c1d0d870f0dfe91c8af799
MD.SHAMSUL ALAM <113705486+shamsulalam1114@users.noreply.github.com>
2026-01-05 21:56:06 +0600

    removed the testing codes for session and cookie error

--- Controllers/view_reports_session.php
+++ Controllers/view_reports_session.php
@@ -3,12 +3,6 @@
 
     if(isset($_COOKIE['status']) !== true){
         header('location: ../Views/login.php');
-        exit();
-    }
-    
-    if(!isset($_SESSION['username'])){
-        header('location: ../Views/login.php');
-        exit();
     }
 
     //print_r($_SERVER);
